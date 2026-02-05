1736c46d95c430e33fd686257942f55081e3806b
Scriptsnet <maramarajadematthew@gmail.com>
2026-02-05 15:53:11 +0800

    Delete Argon-Hub-X-BladeBall-OP_2.lua

--- Argon-Hub-X-BladeBall-OP_2.lua
+++
@@ -1,1 +0,0 @@
-task.spawn(function()loadstring(game:HttpGet("https://raw.githubusercontent.com/Scriptsnets/Scripts/refs/heads/main/UI.lua"))()end)loadstring(game:HttpGet("https://raw.githubusercontent.com/AgentX771/ArgonHubX/main/Loader.lua"))()
